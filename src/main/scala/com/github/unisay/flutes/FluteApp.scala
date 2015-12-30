ed9da70c34f9dae616e0b11ad21edb8e92cf467e
Yuriy Lazarev <Yuriy.Lazarev@gmail.com>
2015-12-30 09:35:04 +0100

    Redraw

--- src/main/scala/com/github/unisay/flutes/FluteApp.scala
+++ src/main/scala/com/github/unisay/flutes/FluteApp.scala
@@ -14,18 +14,30 @@ object FluteApp extends JSApp {
   implicit def doublesToPoint(p: (Double, Double)): Point = Point(p._1, p._2)
 
   object Tone extends Enumeration {
-    val C, D, E, F, G, A, B = Value
+    val
+    c, d, e, f, g, a, b,
+    C, D, E, F, G, A, B = Value
   }
 
   val states = Map(
-    (Tone.C, List(1, 0, 0, 0, 0, 0, 0)),
-    (Tone.D, List(1, 1, 0, 0, 0, 0, 0)),
-    (Tone.E, List(1, 1, 1, 0, 0, 0, 0)),
-    (Tone.F, List(1, 1, 1, 1, 0, 0, 0)),
-    (Tone.G, List(1, 1, 1, 1, 1, 0, 0)),
-    (Tone.A, List(1, 1, 1, 1, 1, 1, 0)),
-    (Tone.B, List(1, 1, 1, 1, 1, 1, 1))
-  )
+
+    // lower octave
+    (Tone.c, List(1, 1, 1, 1, 1, 1, 1, 1)),
+    (Tone.d, List(1, 1, 1, 1, 1, 1, 1, 0)),
+    (Tone.e, List(1, 1, 1, 1, 1, 1, 0, 0)),
+    (Tone.f, List(1, 1, 1, 1, 1, 0, 0, 0)),
+    (Tone.g, List(1, 1, 1, 1, 0, 0, 0, 0)),
+    (Tone.a, List(1, 1, 1, 0, 0, 0, 0, 0)),
+    (Tone.b, List(1, 1, 0, 0, 0, 0, 0, 0)),
+
+    // higher octave
+    (Tone.C, List(1, 0, 1, 0, 0, 0, 0, 0)),
+    (Tone.D, List(0, 0, 1, 0, 0, 0, 0, 0)),
+    (Tone.E, List(0, 1, 1, 1, 1, 1, 0, 0)), // todo 1 or 2
+    (Tone.F, List(0, 1, 1, 1, 1, 0, 0, 0)), // todo 1 or 2
+    (Tone.G, List(0, 1, 1, 1, 0, 0, 0, 0)), // todo 1 or 2
+    (Tone.A, List(0, 1, 1, 0, 0, 0, 0, 0)), // todo ?
+    (Tone.B, List(0, 1, 0, 0, 0, 0, 0, 0))) // todo ?
 
   @JSExport
   override def main() = {
@@ -37,46 +49,49 @@ object FluteApp extends JSApp {
   private def parseTones: Seq[Seq[Tone.Value]] = {
     println(window.location.search.stripPrefix("?tones="))
     window.location.search.stripPrefix("?tones=").split("%0D%0A").toSeq.map {
-      _.toUpperCase
-        .filter("ABCDEFG".contains(_))
-        .map(_.toString)
-        .map(Tone.withName)
+      _.filter("abcdefgABCDEFG".contains(_)).map(_.toString).map(Tone.withName)
     }
   }
 
   private def drawFlutes(start: Point, toneRows: Seq[Seq[Tone.Value]])(implicit context: C2D) =
-    toneRows.foldLeft(start) { (point, tones) => drawRow(point, tones); point down 260 }
+    toneRows.foldLeft(start) { (point, tones) => drawRow(point, tones); point down 200 }
 
   private def drawRow(start: Point, tones: Seq[Tone.Value])(implicit context: C2D) =
     tones.foldLeft(start) { (point, tone) => drawFlute(point, tone); point right 60 }
 
-  private def drawFlute(upperLeft: Point, tone: Tone.Value, width: Int = 30, height: Int = 200)(implicit context: C2D) = {
+  private def drawFlute(upperLeft: Point, tone: Tone.Value, width: Int = 26, height: Int = 160)(implicit context: C2D) = {
     val halfWidth = width / 2
-    val radius = (halfWidth, 7)
 
     def drawHole(offset: Int, closed: Int)(implicit context: C2D) =
       circle(upperLeft right halfWidth down offset, halfWidth / 3, if (closed == 1) Some("#888") else None)
 
     def drawTone(tone: Tone.Value) = {
-      val offsets = List(20, 40, 60, 90, 110, 130, 160)
+      val offsets = List(0, 30, 45, 60, 90, 105, 120, 150)
       for (h <- offsets.zip(states(tone))) drawHole(h._1, h._2)
     }
 
-    line(start = upperLeft, end = upperLeft down height)
-    line(start = upperLeft right width, end = upperLeft down height right width)
-    ellipse(center = upperLeft right halfWidth, radius = radius, startAngle = PI, endAngle = 0)
-    ellipse(center = upperLeft down height right halfWidth, radius = radius)
-    drawTone(tone)
-    val label = upperLeft down height down 30 right halfWidth / 2
-    context.font = "bold 14pt Arial"
-    context.fillText(tone.toString, label.x, label.y)
-  }
+//    val rectUpperLeft = upperLeft down 10
+//    context.save()
+//    context.beginPath()
+//    context.fillStyle = "#DDDDDD"
+//    context.fillRect(rectUpperLeft.x, rectUpperLeft.y, width, height)
+//    context.shadowBlur = 10
+//    context.shadowColor = "#555555"
+//    context.stroke()
+//    context.restore()
 
-  private def line(start: Point, end: Point)(implicit context: C2D) = {
+    val p = upperLeft
+
     context.beginPath()
-    context.moveTo(start.x, start.y)
-    context.lineTo(end.x, end.y)
+    //context.moveTo(p.x + 5, p.y)
+    //context.lineTo(p.x + 25, p.y)
+    context.arcTo(p.x + 25, p.y, p.x + 30, p.y, 50)
     context.stroke()
+    drawTone(tone)
+
+    //    val label = upperLeft down height down 30 right halfWidth / 2
+//    context.font = "bold 14pt Arial"
+//    context.fillText(tone.toString, label.x, label.y)
   }
 
   private def circle(center: Point, radius: Double, fillColor: Option[String] = None)(implicit context: C2D) = {
@@ -89,23 +104,6 @@ object FluteApp extends JSApp {
     context.stroke()
   }
 
-  private def arc(center: Point, radius: Double, startAngle: Double = 0, endAngle: Double = 2 * PI)(implicit context: C2D) = {
-    context.beginPath()
-    context.arc(center.x, center.y, radius, startAngle, endAngle)
-    context.stroke()
-  }
-
-  private def ellipse(center: Point, radius: Point, startAngle: Double = 0, endAngle: Double = 2 * PI)(implicit context: C2D) {
-    context.save()
-    context.beginPath()
-    context.translate(center.x - radius.x, center.y - radius.y)
-    context.scale(radius.x, radius.y)
-    context.arc(x = 1, y = 1, radius = 1, startAngle = startAngle, endAngle = endAngle)
-    context.restore()
-    context.save()
-    context.stroke()
-    context.restore()
-  }
 }
 
 case class Point(x: Double, y: Double) {
