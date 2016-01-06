f5c8efac9836234881abdd3090d2ab67d52bf09c
Yuriy Lazarev <Yuriy.Lazarev@gmail.com>
2016-01-06 19:03:42 +0100

    New simpler look with gradient

--- src/main/scala/com/github/unisay/flutes/FluteApp.scala
+++ src/main/scala/com/github/unisay/flutes/FluteApp.scala
@@ -40,139 +40,84 @@ object FluteApp extends JSApp {
     (Tone.A, List(0, 1, 1, 0, 0, 0, 0, 0)), // todo ?
     (Tone.B, List(0, 1, 0, 0, 0, 0, 0, 0))) // todo ?
 
+  val fluteWidth = 30
+  val fluteHeight = 160
+
+  @JSExport
+  def resizeCanvas(canvas: html.Canvas)(implicit context: C2D): Unit = {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    drawFlutes((15, 25), parseTones)
+  }
+
   @JSExport
   override def main() = {
     val canvas = dom.document.getElementById("fluteCanvas").asInstanceOf[html.Canvas]
     implicit val context2D = canvas.getContext("2d").asInstanceOf[C2D]
-//     drawFlutes((5, 20), parseTones)
-    drawFlute((20, 20), Tone.C)
+    val render = (_: Event) ⇒ resizeCanvas(canvas)
+    window.addEventListener("resize", render, useCapture = false)
+    render(null)
   }
 
   private def parseTones: Seq[Seq[Tone.Value]] = {
-    println(window.location.search.stripPrefix("?tones="))
     window.location.search.stripPrefix("?tones=").split("%0D%0A").toSeq.map {
       _.filter("abcdefgABCDEFG".contains(_)).map(_.toString).map(Tone.withName)
     }
   }
 
   private def drawFlutes(start: Vector2d, toneRows: Seq[Seq[Tone.Value]])(implicit context: C2D) =
-    toneRows.foldLeft(start) { (point, tones) => drawRow(point, tones); point down 200 }
+    toneRows.foldLeft(start) { (point, tones) => drawRow(point, tones); point down fluteHeight down 40 }
 
   private def drawRow(start: Vector2d, tones: Seq[Tone.Value])(implicit context: C2D) =
-    tones.foldLeft(start) { (point, tone) => drawFlute(point, tone); point right 60 }
+    tones.foldLeft(start) { (point, tone) => drawFlute(point, tone); point right fluteWidth * 2 }
 
-  private def drawFlute(upperLeft: Vector2d, tone: Tone.Value, width: Double = 26, height: Double = 160)(implicit context: C2D) = {
-    val halfWidth = width / 2
+  private def drawFlute(upperLeft: Vector2d, tone: Tone.Value)(implicit context: C2D) = {
+    context.strokeStyle = "black"
 
-    def drawHole(offset: Double, closed: Int)(implicit context: C2D) =
-      circle(upperLeft right halfWidth down offset, halfWidth / 3, if (closed == 1) Some("#888") else None)
+    def drawHole(v: Vector2d, closed: Int, radius: Double = 4.5)(implicit context: C2D) =
+      circle(v, radius, if (closed == 1) Some("#222") else None)
 
-    def drawTone(tone: Tone.Value) = {
-      val offsets = List(0, 30, 45, 60, 90, 105, 120, 150)
-      for (h <- offsets.zip(states(tone))) drawHole(h._1, h._2)
+    def drawHoles(v: Vector2d, tone: Tone.Value) = {
+      val ss = states(tone).iterator
+      drawHole(v up 15, ss.next(), 3)
+      drawHole(v down 15, ss.next())
+      drawHole(v down 35, ss.next())
+      drawHole(v down 55, ss.next())
+      drawHole(v down 80, ss.next())
+      drawHole(v down 100, ss.next())
+      drawHole(v down 120, ss.next())
+      drawHole(v down 140, ss.next())
     }
 
-    def drawMund(s: Vector2d, height: Double, topWidth: Double, bottomWidth: Double)(implicit context: C2D) = {
-      val deltaWidth = bottomWidth - topWidth
-      val topLeft = s right deltaWidth / 2
-      val topRight = topLeft right topWidth
-      val bottomLeft = s down height
-      val bottomRight = bottomLeft right bottomWidth
+    val downLeft = upperLeft down fluteHeight
+    val upperRight = upperLeft right fluteWidth
+    val downRight = upperRight down fluteHeight
+    val vShift = 10
 
-      strokePath { implicit context ⇒
-        moveTo(topLeft)
-        lineTo(topRight)
-        bezierTo(topRight down 5, bottomRight up height / 2 right 5, bottomRight)
-        moveTo(bottomLeft)
-        bezierTo(bottomLeft up height / 2 left 5, topLeft down 5, topLeft)
-      }
+    strokePath { _ ⇒
+      line(upperLeft, downLeft)
+      bezierTo(downLeft up vShift, downRight up vShift, downRight)
+      lineTo(upperRight)
+      bezierTo(upperRight up vShift, upperLeft up vShift, upperLeft)
+
+      fillGradient(upperLeft, upperRight, (0.05, "#DDDDDD"), (0.50, "white"), (0.95, "#DDDDDD"))
     }
 
-    def drawRing(s: Vector2d, width: Double, height: Double, sideRadius: Double = 4, topRadius: Double = 6)(implicit context: C2D) = {
-      strokePath { implicit context ⇒
-        moveTo(s)
-        quadraticTo(s left sideRadius down height / 2, s down height)
-        quadraticTo(s right width / 2 down height + topRadius, s right width down height)
-        quadraticTo(s right width + sideRadius down height / 2, s right width)
-        quadraticTo(s right width / 2 down topRadius, s)
-      }
+    strokePath { _ ⇒
+      moveTo(downLeft)
+      bezierTo(downLeft up vShift, downRight up vShift, downRight)
+      bezierTo(downRight down vShift, downLeft down vShift, downLeft)
+
+      fillGradient(downLeft up vShift, downLeft down vShift, (0.35, "white"), (0.95, "darkgray"))
     }
 
-    def drawTop(s: Vector2d, height: Double, topWidth: Double, bottomWidth: Double)(implicit context: C2D) = {
-      strokePath { implicit context ⇒
-        moveTo(s)
-        lineTo(s right topWidth - bottomWidth down height)
-        moveTo(s right bottomWidth down height)
-        lineTo(s right topWidth)
-      }
-    }
+    drawHoles(upperLeft right fluteWidth / 2, tone)
+  }
 
-    def drawAirHole(topLeft: Vector2d, width: Double, height: Double, radius: Double = .8)(implicit context: C2D) = {
-      val topCenter = topLeft right (width / 2)
-      val bottomCenter = topCenter down height
-      val topRight = topLeft right width
-      val delta = 0.3
-      val bottomLeft = topLeft down height right delta
-      val bottomRight = topRight down height left delta
-
-      val cutHeight = 3 * height
-      val cutDelta = 2
-
-      strokePath { implicit context ⇒
-        moveTo(topLeft)
-        lineTo(topLeft down cutHeight left cutDelta)
-        quadraticTo(bottomCenter down cutHeight down 3 * radius, topRight down cutHeight right cutDelta)
-        lineTo(topRight)
-      }
-
-      strokePath { implicit context ⇒
-        moveTo(topLeft)
-        quadraticTo(topCenter down radius, topRight)
-        lineTo(bottomRight)
-        quadraticTo(bottomCenter down radius, bottomLeft)
-        lineTo(topLeft)
-        fill("#222")
-      }
-    }
-
-    val mundStart = upperLeft
-    val mundHeight = 30
-    val mundTopWidth = 15
-    val mundBottomWidth = 26
-
-    drawMund(mundStart, mundHeight, mundTopWidth, mundBottomWidth)
-
-    val ringStart = mundStart down mundHeight
-    val ringWidth = mundBottomWidth
-    val ringHeight = 6
-
-    drawRing(ringStart, ringWidth, ringHeight)
-
-    val topStart = ringStart down ringHeight
-    val topHeight = 60
-    val topTopWidth = ringWidth
-    val topWidthDelta = 4
-    val topBottomWidth = topTopWidth - topWidthDelta
-
-    drawTop(topStart, topHeight, topTopWidth, topBottomWidth)
-
-    val holeWidth = topTopWidth / 3
-    val holeHeight = 5
-
-    drawAirHole(topStart right (topTopWidth - holeWidth) / 2 down 3, holeWidth, holeHeight)
-
-    val ring2Start = topStart down topHeight right topWidthDelta
-    val ring2Width = topBottomWidth - topWidthDelta
-    val ring2Height = ringHeight
-
-    drawRing(ring2Start, ring2Width, ring2Height)
-
-
-//    drawTone(tone)
-
-    //    val label = upperLeft down height down 30 right halfWidth / 2
-    //    context.font = "bold 14pt Arial"
-    //    context.fillText(tone.toString, label.x, label.y)
+  private def fillGradient(g1: Vector2d, g2: Vector2d, colorStops: (Double, String)*)(implicit context: C2D) = {
+    val gradient = context.createLinearGradient(g1.x, g1.y, g2.x, g2.y)
+    colorStops.foreach(stop ⇒ gradient.addColorStop(stop._1, stop._2))
+    fill(gradient)
   }
 
   private def circle(center: Vector2d, radius: Double, fillColor: Option[String] = None)(implicit context: C2D) = {
@@ -182,12 +127,14 @@ object FluteApp extends JSApp {
     }
   }
 
-
   private def fill(style: String)(implicit context: C2D) = {
-    val backup = context.fillStyle
     context.fillStyle = style
     context.fill()
-    context.fillStyle = backup
+  }
+
+  private def fill(gradient: CanvasGradient)(implicit context: C2D) = {
+    context.fillStyle = gradient
+    context.fill()
   }
 
   private def moveTo(v: Vector2d)(implicit context: C2D, scale: Scale) =
@@ -196,10 +143,10 @@ object FluteApp extends JSApp {
   private def lineTo(v: Vector2d)(implicit context: C2D, scale: Scale) =
     context.lineTo(v.x * scale.x, v.y * scale.y)
 
-  private def fillRect(v: Vector2d, wh: Vector2d)(implicit context: C2D, scale: Scale) =
-    context.fillRect(
-      v.x * scale.x, v.y * scale.y,
-      wh.x * scale.x, wh.y * scale.y)
+  private def line(from: Vector2d, to: Vector2d)(implicit context: C2D, scale: Scale) = {
+    moveTo(from)
+    lineTo(to)
+  }
 
   private def bezierTo(c1: Vector2d, c2: Vector2d, to: Vector2d)(implicit context: C2D) =
     context.bezierCurveTo(
@@ -207,14 +154,10 @@ object FluteApp extends JSApp {
       c2.x * scale.x, c2.y * scale.y,
       to.x * scale.x, to.y * scale.y)
 
-  private def quadraticTo(c: Vector2d, to: Vector2d)(implicit context: C2D) =
-    context.quadraticCurveTo(
-      c.x * scale.x, c.y * scale.y,
-      to.x * scale.x, to.y * scale.y)
-
   private def strokePath(f: C2D => Unit)(implicit context: C2D) = {
     context.beginPath()
     f(context)
+    context.closePath()
     context.stroke()
   }
 
